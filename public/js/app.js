af7f517c3fce2ecd5edb4805c294af28dd55532f
Noah Wimmer <nwimmer123@yahoo.com>
2016-06-28 13:54:25 -0700

    carosel works and fomatted

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,11 @@
 $(document).ready(function() {
-
   $('#myCarousel').carousel({
-    interval: 10000
+  interval: 10000
   })
-
-  $('.carousel .item').each(function(){
-      var next = $(this).next();
-
-      if (!next.length) {
-          next = $(this).siblings(':first');
-      }
-
-      next.children(':first-child').clone().appendTo($(this));
-
-      if (next.next().length>0) {
-          next.next().children(':first-child').clone().appendTo($(this));
-      }
-      else {
-          $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-      }
+    
+    $('#myCarousel').on('slid.bs.carousel', function() {
+      //alert("slid");
   });
-
+    
+    
 });
