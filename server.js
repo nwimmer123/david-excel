9b4d767ea4c1c8c2ea5767ea5249983ad9e416e8
Noah Wimmer <nwimmer123@yahoo.com>
2016-06-30 10:15:22 -0700

    faq route working

--- server.js
+++ server.js
@@ -2,18 +2,8 @@
 
 var express = require('express');
 var app = express();
-var flash = require('connect-flash');
 
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-
-var html = require('html');
-
-
-// serve static files from public folder
 app.use('/',express.static(__dirname + '/public'));
-app.use('/',express.static(__dirname + '/views'));
 
 app.set('view engine', 'ejs');
 
@@ -23,13 +13,10 @@ app.get('/', function(req,res) {
   res.render('pages/index');
 });
 
-// app.get('/faq', function(req,res) {
-//   res.send('pages/faq');
-// });
-
-app.get('/faq', function homepage (req, res) {
-  res.sendFile(__dirname + 'pages/faq.ejs');
+app.get('/faq', function(req,res) {
+  res.render('pages/faq');
 });
+
 
 // SERVER 
 
