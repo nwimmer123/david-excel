0e72f3d3268576c936a69ea6f81f59d30a6fd2a9
Noah Wimmer <nwimmer123@yahoo.com>
2016-07-03 16:06:31 -0700

    working click on/off faq's

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,24 @@
 $(document).ready(function() {
+
   $('#myCarousel').carousel({
   interval: 10000
   })
     
-    $('#myCarousel').on('slid.bs.carousel', function() {
+  $('#myCarousel').on('slid.bs.carousel', function() {
       //alert("slid");
+  });
+
+  $('.faq_question').click(function() {
+   
+    if ($(this).parent().is('.open')){
+      $(this).closest('.faq').find('.faq_answer_container').animate({'height':'0'},500);
+      $(this).closest('.faq').removeClass('open');
+
+      }else{
+        var newHeight =$(this).closest('.faq').find('.faq_answer').height() +'px';
+        $(this).closest('.faq').find('.faq_answer_container').animate({'height':newHeight},500);
+        $(this).closest('.faq').addClass('open');
+      }
   });
     
     
